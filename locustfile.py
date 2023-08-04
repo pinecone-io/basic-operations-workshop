c190debaf263a381856fb114319e966799f8c530
cfossguy <cfossguy@gmail.com>
2023-08-04 17:46:42 -0400

    fixed locust script so that it does not fail when there are an acceptable number of errors

--- locustfile.py
+++ locustfile.py
@@ -42,6 +42,15 @@ for img in test_dataset:
         embedding = model.encode_image(image_pp).squeeze().tolist()
         test_vectors.append({'embedding': embedding,
                         'description': label_descriptions[img["label"]]})
+print(f"Generated embeddings for {len(test_vectors)} random test images")
+
+@ev.quitting.add_listener
+def _(environment, **kw):
+    if environment.stats.total.fail_ratio > 0.20:
+        print("Test failed due to failure ratio > 20%")
+        environment.process_exit_code = 1
+    else:
+        environment.process_exit_code = 0
 
 class PineconeUser(HttpUser):
     
@@ -50,42 +59,39 @@ class PineconeUser(HttpUser):
     @task
     def query_pinecone(self):
 
-        top_k_hit = False
+            # Select a random image+embedding from the test dataset
+            test_vector = random.choice(test_vectors)
 
-        # Select a random image+embedding from the test dataset
-        test_vector = random.choice(test_vectors)
+            start_time = time.time()
+            query_result = index.query(
+                vector = test_vector['embedding'],
+                namespace=namespace,
+                top_k=top_k,
+                include_values=False,
+                include_metadata=True
+            )
+            end_time = time.time()
 
-        start_time = time.time()
-        query_result = index.query(
-            vector = test_vector['embedding'],
-            namespace=namespace,
-            top_k=top_k,
-            include_values=False,
-            include_metadata=True
-        )
-        end_time = time.time()
+            response_time = (end_time - start_time) * 1000
 
-        response_time = (end_time - start_time) * 1000
-        for match in query_result.matches:
-            if match.metadata['description'] == test_vector['description']:
-                top_k_hit = True
-        
-        if top_k_hit == True:
-            ev.request.fire(request_type="grpc",
-                        name="query_pinecone",
-                        response_time=response_time,
-                        response_length=top_k,
-                        exception=None
-                    )
-        else:
-            ev.request.fire(request_type="grpc",
-                        name="query_pinecone",
-                        response_time=response_time,
-                        response_length=top_k,
-                        exception="Top K miss",
-                        context={}
-                    )
-            
+            my_list = query_result.matches
+            descriptions = [entry['metadata']['description'] for entry in my_list]
+            most_common_item = max(set(descriptions), key=descriptions.count)
 
+            try:
+                assert most_common_item == test_vector['description']
+                ev.request.fire(request_type="grpc",
+                            name="query_pinecone",
+                            response_time=response_time,
+                            response_length=top_k,
+                            exception=None
+                        )
+            except:
+                ev.request.fire(request_type="grpc",
+                            name="query_pinecone",
+                            response_time=response_time,
+                            response_length=top_k,
+                            exception="Most common item does not match"
+                        )
+                
 
-        
